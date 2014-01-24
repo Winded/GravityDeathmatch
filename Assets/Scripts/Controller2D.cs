51526b00d02ffe719707852404d2362d274c7ba9
Winded <hytonen-a@hotmail.com>
2014-01-24 23:56:48 +0200

    Fix octagon collider

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller2D : MonoBehaviour
+{
+
+    
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+}
