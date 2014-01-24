946d6133280d3ec3ca92baba21d65363b0c2f8e8
Timo Kellomaki <dae@iki.fi>
2014-01-25 01:53:47 +0200

    First gravity demo working

---
+++ Assets/Scripts/daemou/GravityControllerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityControllerScript : MonoBehaviour {
+
+	public Transform[] bullets = new Transform[2];
+
+	// Use this for initialization
+	void Start () {
+		bullets [0].GetComponent<GravityAreaScript> ().gravityDir = new Vector2 (0, -1);
+	}
+
+	public void SetGravity(int player) {
+		Debug.Log ("gravity setting");
+		Vector2 pos = bullets [player].transform.position;
+		Debug.Log ("Finding collider for " + pos);
+		Collider2D collider = Physics2D.OverlapPoint (pos, 1 << 8);
+		//if (Physics.Raycast (pos, new Vector3(0.0f, 0.0f, -1.0f), out hit) ) {
+		if ( collider != null ) {
+			bullets [player].GetComponent<GravityAreaScript> ().gravityDir = collider.GetComponent<GravityAreaScript> ().gravityDir;
+			Debug.Log ("Collider: " + collider.name);
+			Debug.Log ("Gravity now " + bullets [player].GetComponent<GravityAreaScript> ().gravityDir );
+		} else{
+			Debug.Log ("Gravity raycast missed!");
+		}
+	}
+
+	void FixedUpdate()
+	{
+		bullets [0].rigidbody2D.AddForce (10.0f * bullets [0].GetComponent<GravityAreaScript> ().gravityDir.normalized);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/daemou/ThrowScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowScript : MonoBehaviour {
+
+	//public Transform bulletTestPrefab;
+	public Camera thecamera;
+	public Transform gravityController;
+
+	public Transform bullet;
+
+	bool dragging;
+	Vector2 startPos;
+	// Use this for initialization
+	void Start () {
+		dragging = false;
+	}
+
+	void Shoot(Vector2 from, Vector2 delta) {
+		bullet.transform.position = new Vector3 (from.x, from.y, 0.0f);
+		bullet.rigidbody2D.AddForce ( new Vector2(100.0f * delta.x, 100.0f * delta.y) );
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetMouseButtonDown (0) && !dragging ) {
+			dragging = true;
+			startPos = thecamera.ScreenToWorldPoint(Input.mousePosition);
+			//Debug.Log ("down at " + startPos );
+		} else if ( Input.GetMouseButtonUp(0) && dragging) {
+			dragging = false;
+			Vector2 liftPos = thecamera.ScreenToWorldPoint(Input.mousePosition);
+			//Debug.Log ( "up at" + Input.mousePosition );
+			Shoot(startPos, liftPos - startPos);
+		}
+
+		if (Input.GetKeyDown ("space")) 
+		{
+			gravityController.GetComponent<GravityControllerScript>().SetGravity(0);
+		}
+
+	}
+}
