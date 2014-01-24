d0e37a90c20ded79bd57b9546b7ec6da62d65c9d
Timo Kellomaki <dae@iki.fi>
2014-01-24 23:59:35 +0200

    Throw-testailua

---
+++ Assets/Scripts/ThrowScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowScript : MonoBehaviour {
+
+	bool dragging;
+	Vector2 startPos;
+	// Use this for initialization
+	void Start () {
+		dragging = false;
+	}
+
+	void shoot(Vector2 from, Vector2 delta) {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetMouseButtonDown (0) && !dragging ) {
+			dragging = true;
+			startPos = Input.mousePosition;
+			Debug.Log ("down at " + startPos );
+		} else if ( Input.GetMouseButtonUp(0) && dragging) {
+			dragging = false;
+			shoot(startPos, Input.mousePosition - startPos);
+		}
+
+	}
+}
