4260d6650f24a8a590a2b974c7eeda48a23be3a6
Winded <hytonen-a@hotmail.com>
2014-01-25 15:51:58 +0200

    Added forbidden zone

---
+++ Assets/Scripts/ForbiddenZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class ForbiddenZone : MonoBehaviour
+{
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        var other = col.collider;
+        if (other.gameObject.tag == "Player")
+        {
+            var norm = col.contacts.First().normal;
+            other.transform.position += (Vector3)norm*2;
+            print("YAY");
+        }
+    }
+
+}
