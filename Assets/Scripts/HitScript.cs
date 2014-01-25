75f02dcab5a9d8897227dafcb27fccc14b04c138
EskoVankka <esko.vankka@uta.fi>
2014-01-25 23:30:30 +0200

    taas uus yritys

--- Assets/Scripts/HitScript.cs
+++ Assets/Scripts/HitScript.cs
@@ -2,41 +2,32 @@
 using System.Collections;
 
 public class HitScript : MonoBehaviour {
-
+	
 	public int lives;
 	public int fullHealth;
 	public float damageMultiplier;
 	public float armor;
-    public ParticleSystem BloodSpill;
 
 	public int health;
-
+	
 	// Use this for initialization
-<<<<<<< HEAD
 	void Start () {
 		health = fullHealth;
-
+		
 	}
-=======
-    private void Start()
-    {
-        health = fullHealth;
-    }
->>>>>>> 6bc63f88186f0a1b2a9d5dbebbde211226933ea8
-
-    void OnCollisionEnter2D(Collision2D collision)
+	
+	void OnCollisionEnter2D(Collision2D collision)
 	{
 		if ( collision.collider.gameObject.layer == LayerMask.NameToLayer("Bullet") )
 		{
 			var mag = collision.relativeVelocity.magnitude;
-			if (mag > armor)
+			if ( mag > armor) 
 			{
 				//Esko
 				transform.GetComponentInChildren<PlayerAnimatorControllerScript>().gotHit = true;
 				//Esko
-
+				
 				health -= (int)(damageMultiplier * (mag - armor));
-<<<<<<< HEAD
 				Debug.Log ( "HEALTH:" + health );
 				//Esko
 				if (health <= 0)
@@ -46,21 +37,17 @@ public class HitScript : MonoBehaviour {
 					{
 						;
 					}
-				
+					
 				}
 				//Esko
-=======
-			    BloodSpill.transform.position = collision.contacts[0].point;
-			    BloodSpill.Play();
->>>>>>> 6bc63f88186f0a1b2a9d5dbebbde211226933ea8
 			}
 		}
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		if (health <= 0) {
-
+			
 			Debug.Log ("KILLED!!!!");
 			lives--;
 			health = fullHealth;
@@ -71,6 +58,6 @@ public class HitScript : MonoBehaviour {
 			Destroy (transform.gameObject);
 		}
 	}
-
-
+	
+	
 }
