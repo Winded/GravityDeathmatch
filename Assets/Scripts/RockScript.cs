5513f419c829d006aa597a7e02a52f1e61f3aaa1
Winded <hytonen-a@hotmail.com>
2014-01-26 12:19:31 +0200

    MOAR ROCKS

---
+++ Assets/Scripts/RockScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockScript : MonoBehaviour
+{
+
+    public float VelocityMagnitude = 1f;
+
+    // Use this for initialization
+    private void Start()
+    {
+        var rndAng = Random.Range(-180f, 180f);
+        var euler = transform.eulerAngles;
+        euler.z = rndAng;
+        transform.eulerAngles = euler;
+        var vDir = transform.right;
+        rigidbody2D.velocity = vDir*VelocityMagnitude;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
